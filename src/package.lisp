d9de786742c86d08d5195fcfff2d8dc47365df04
Phil Newton <phil@sodaware.net>
2016-09-11 07:32:24 -0400

    Export error constants

--- src/package.lisp
+++ src/package.lisp
@@ -9,7 +9,7 @@
            ;; Conditions
            #:deprecated-method
            #:deprecated-parameter
-           
+
            ;; Client object
            #:client
            #:make-client
@@ -22,4 +22,38 @@
            #:add-multisig-address
            #:backup-wallet
            #:create-multisig-address
-           #:server-balance))
+           #:server-balance
+
+           ;; Error Constants
+           #:+rpc-invalid-request+
+           #:+rpc-method-not-found+
+           #:+rpc-invalid-params+
+           #:+rpc-internal-error+
+           #:+rpc-parse-error+
+           #:+rpc-misc-error+
+           #:+rpc-forbidden-by-safe-mode+
+           #:+rpc-type-error+
+           #:+rpc-invalid-address-or-key+
+           #:+rpc-out-of-memory+
+           #:+rpc-invalid-parameter+
+           #:+rpc-database-error+
+           #:+rpc-deserialization-error+
+           #:+rpc-verify-error+
+           #:+rpc-verify-rejected+
+           #:+rpc-verify-already-in-chain+
+           #:+rpc-in-warmup+
+           #:+rpc-client-not-connected+
+           #:+rpc-client-in-initial-download+
+           #:+rpc-client-node-already-added+
+           #:+rpc-client-node-not-added+
+           #:+rpc-client-node-not-connected+
+           #:+rpc-client-invalid-ip-or-subnet+
+           #:+rpc-wallet-error+
+           #:+rpc-wallet-insufficient-funds+
+           #:+rpc-wallet-invalid-account-name+
+           #:+rpc-wallet-keypool-ran-out+
+           #:+rpc-wallet-unlock-needed+
+           #:+rpc-wallet-passphrase-incorrect+
+           #:+rpc-wallet-wrong-enc-state+
+           #:+rpc-wallet-encryption-failed+
+           #:+rpc-wallet-already-unlocked+))
